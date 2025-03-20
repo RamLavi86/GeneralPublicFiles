dea02c2ef951c9803bb3db4cb9269bfd3d79b065
idanrazisrdev <idanraz2015@gmail.com>
2025-03-20 17:12:07 +0200

    Update sg_ready_site_list_12_24.py
    Updated list 20March_2025

--- sg_ready_site_list_12_24.py
+++ sg_ready_site_list_12_24.py
@@ -3,5 +3,8 @@ site_id_list=[
     4314184, # Bakker Residence V2    
     3572142, # MFE SEP NA
     4453528, # MSE Uros Pahor
+    2009706, # J16b
+    855119, # Andre Schoneis
+    2550334, # V1 Musterhauspark 
     1358897 # Wagner Solar Hungária Kft
     ]
