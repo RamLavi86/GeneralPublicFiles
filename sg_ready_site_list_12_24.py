bd3dc0fcb69be8328a1af538c29fd609f6b79274
idanrazisrdev <idanraz2015@gmail.com>
2025-04-01 17:27:27 +0300

    Update sg_ready_site_list_12_24.py
    Added Site-ID 3897804 MSE Kumer Irena

--- sg_ready_site_list_12_24.py
+++ sg_ready_site_list_12_24.py
@@ -4,6 +4,7 @@ site_id_list=[
     3572142, # MFE SEP NA
     4453528, # MSE Uros Pahor
     2009706, # J16b
-    2550334, # V1 Musterhauspark 
+    2550334, # V1 Musterhauspark
+    3897804, # MSE Kumer Irena
     1358897 # Wagner Solar Hungária Kft
     ]
